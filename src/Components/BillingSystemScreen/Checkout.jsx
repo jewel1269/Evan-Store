58ccc238a6bba429e5ab1222f6894e4d321ea0d1
Jewel Mia <145703213+jewel1269@users.noreply.github.com>
2024-11-01 17:54:31 +0600

    uri changes

--- src/Components/BillingSystemScreen/Checkout.jsx
+++ src/Components/BillingSystemScreen/Checkout.jsx
@@ -60,7 +60,7 @@ function Checkout() {
     };
 
     try {
-      await axios.post("http://localhost:5000/api/orders", orderData);
+      await axios.post("http://localhost:5000/order/api/orders", orderData);
       toast.success("Order Successfully Complete");
       navigate("/thankyou");
       removeItem(items)
